3244c31591312cb3b4898fb89cca905c8e544993
Travis Sein <tsein@stripe.com>
2020-06-10 12:45:03 -0700

    semicolons

--- key-leaks/main.js
+++ key-leaks/main.js
@@ -3,9 +3,9 @@ let fake_key_1 = 'sk_live_aaaaaaaaaaaaaaaaaaaaaaaa';
 console.log(fake_key_1);
 
 // Test revoked keys
-let fake_key_2 = 'sk_live_AlchMwrhXHiyeq2RuiVAISlS00h7rNX51T'
+let fake_key_2 = 'sk_live_AlchMwrhXHiyeq2RuiVAISlS00h7rNX51T';
 console.log(fake_key_2);
 
 // Test non-working but valid keys
-let fake_key_3 = 'sk_live_SmbEH34pIy1fA7OIpv8ecOZS00U1fjEpHi'
+let fake_key_3 = 'sk_live_SmbEH34pIy1fA7OIpv8ecOZS00U1fjEpHi';
 console.log(fake_key_3);
