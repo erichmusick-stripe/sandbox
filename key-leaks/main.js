c5d57fd97240d941b4e8d5f3398739391c287ddf
Travis Sein <tsein@stripe.com>
2020-06-10 12:44:42 -0700

    Test revoked keys

--- key-leaks/main.js
+++ key-leaks/main.js
@@ -2,6 +2,10 @@
 let fake_key_1 = 'sk_live_aaaaaaaaaaaaaaaaaaaaaaaa';
 console.log(fake_key_1);
 
-// Test non-working but valid keys
+// Test revoked keys
 let fake_key_2 = 'sk_live_AlchMwrhXHiyeq2RuiVAISlS00h7rNX51T'
 console.log(fake_key_2);
+
+// Test non-working but valid keys
+let fake_key_3 = 'sk_live_SmbEH34pIy1fA7OIpv8ecOZS00U1fjEpHi'
+console.log(fake_key_3);
