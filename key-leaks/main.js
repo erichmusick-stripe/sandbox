c496053d2b16fd89b9ae07fa393ecaa736b49267
Travis Sein <tsein@stripe.com>
2020-06-10 12:23:17 -0700

    Test non-working but valid keys

--- key-leaks/main.js
+++ key-leaks/main.js
@@ -1,3 +1,7 @@
 // Test strings that look like valid keys
-let fake_key = 'sk_live_aaaaaaaaaaaaaaaaaaaaaaaa';
-console.log(fake_key);
+let fake_key_1 = 'sk_live_aaaaaaaaaaaaaaaaaaaaaaaa';
+console.log(fake_key_1);
+
+// Test non-working but valid keys
+let fake_key_2 = 'sk_live_AlchMwrhXHiyeq2RuiVAISlS00h7rNX51T'
+console.log(fake_key_2);
